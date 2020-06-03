15c3b1eba0eecf9d2750840d6bb8405fdc5bd88b
Moordown <pappy.mot@gmail.com>
2020-06-04 00:49:21 +0500

    add phys to train

---
+++ Assets/Scripts/Physics/PushInTie.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PushInTie : MonoBehaviour
+{
+    private Transform target;
+    private Vector3 push;
+
+    private Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (target is null) return;
+        rigidbody.AddForce(push, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var tied = other.gameObject.GetComponent<PushInTied>();
+        if (tied is null) return;
+        target = tied.GetComponent<Transform>();
+        push = tied.PushVector;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        var tied = other.gameObject.GetComponent<PushInTied>();
+        if (tied is null) return;
+        target = null;
+    }
+}
---
+++ Assets/Scripts/Physics/PushOutTie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PushOutTie : MonoBehaviour
+{
+    private Transform target;
+    private Vector3 push;
+
+    private Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (target is null) return;
+        rigidbody.AddForce(push, ForceMode.Impulse);
+        Debug.Log("use");
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var tied = other.gameObject.GetComponent<PushOutTied>();
+        if (tied is null) return;
+        Debug.Log("Enter pushouttied");
+        target = null;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        var tied = other.gameObject.GetComponent<PushOutTied>();
+        if (tied is null) return;
+        target = tied.GetComponent<Transform>();
+        push = tied.PushVector;
+        Debug.Log($"Exit pushouttied with: {push}");
+    }
+}
